b8c0a3d8cf5772e95dbc73c6142fbdb45c424763
Jordan <jordanmand@gmail.com>
2016-12-16 23:03:22 -0600

    Update asset url for staging

--- resources/views/partial/headers.blade.php
+++ resources/views/partial/headers.blade.php
@@ -5,7 +5,7 @@
 	<link rel="stylesheet" href="{{ asset('/css/vendor/bootstrap.min.css') }}">
 	<link rel="stylesheet" href="{{ asset('/css/vendor/normalize.css') }}">
 	<link rel="stylesheet" href="{{ asset('/css/vendor/font-awesome-4.7.0/css/font-awesome.min.css') }}">
-	<link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+	<link rel="stylesheet" href="/starsystems/public{{ elixir('css/app.css') }}">
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Corben:bold">
 @show
 
@@ -13,5 +13,5 @@
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 	<script src="{{ asset('/js/vendor/bootstrap.min.js') }}" async></script>
 	<script src="{{ asset('/js/vendor/jquery.touchSwipe.min.js') }}"></script>
-	<script src="{{ elixir('js/app.js') }}"></script>
+	<script src="/starsystems/public{{ elixir('js/app.js') }}"></script>
 @show
