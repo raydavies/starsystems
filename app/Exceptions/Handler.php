9c619d6f6636c57be817432ffdd29800aa8edcc3
Jordan <jordanmand@gmail.com>
2016-12-17 01:00:39 -0600

    Ensure default error page displayed on non-dev environments

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\App;
 
 class Handler extends ExceptionHandler {
 
@@ -36,7 +37,11 @@ class Handler extends ExceptionHandler {
 	 */
 	public function render($request, Exception $e)
 	{
-		return parent::render($request, $e);
+	    if (App::environment('local')) {
+            return parent::render($request, $e);
+        } else {
+	        return response()->view('errors.500');
+        }
 	}
 
 }
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -5,8 +5,10 @@
 @section('page_content')
     <div class="container">
         <div class="row clearfix">
-            <h1 class="header header-xl">500 Server Error</h1>
-            <p class="col-md-6 text-center font-lrg">Looks like something went wrong! Please feel free to report a bug to our support team <a href="{{ route('contact') }}">here</a>.</p>
+            <div class="col-md-6">
+                <h1 class="header header-xl">500 Server Error</h1>
+                <p class="col-md-6 text-center font-lrg">Looks like something went wrong! Please feel free to report a bug to our support team <a href="{{ route('contact') }}">here</a>.</p>
+            </div>
         </div>
     </div>
 @stop
