06c00216b00cd83a37a802beebbc6ad0000e19c3
Jordan <jordanmand@gmail.com>
2017-05-04 14:31:03 -0500

    Add BBB seal to footer

--- resources/views/partial/headers.blade.php
+++ resources/views/partial/headers.blade.php
@@ -3,7 +3,7 @@
 
 @section('headstyles')
 	<link rel="stylesheet" href="{{ asset('/css/vendor/bootstrap.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('/css/vendor/normalize.css') }}">
+	<link rel="stylesheet" href="{{ asset('/css/vendor/normalize.min.css') }}">
 	<link rel="stylesheet" href="{{ asset('/css/vendor/font-awesome-4.7.0/css/font-awesome.min.css') }}">
 	<link rel="stylesheet" href="{{ elixir('css/app.css') }}">
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Corben:bold">
@@ -12,6 +12,6 @@
 @section('headscripts')
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 	<script src="{{ asset('/js/vendor/bootstrap.min.js') }}" async></script>
-	<script src="{{ asset('/js/vendor/jquery.touchSwipe.min.js') }}"></script>
-	<script src="{{ elixir('js/app.js') }}"></script>
+	<script src="{{ asset('/js/vendor/jquery.touchSwipe.min.js') }}" async></script>
+	<script src="{{ elixir('js/app.js') }}" async></script>
 @show
